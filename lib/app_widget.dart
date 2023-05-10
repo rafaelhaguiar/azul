3cf30ea3b5cc1d861747484b8465f7e8f9f2e17a
Rafael Aguiar <rafaelhenriqueggt@gmail.com>
2023-05-10 19:26:39 -0300

    adicionada home page

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:verde/home_page.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      home: HomePage(),
+    );
   }
 }
